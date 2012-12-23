0bb6f0488828a885adeb8cd83ca4f69e60a5d2f4
rhyok <ragmanme@gmail.com>
2012-12-23 00:03:12 -0800

    ***hostSettings CHANGED***
    models.py:
     - Updated Player and Guest models to pull data from the Django user authentication system for user purposes.
     - Updated Tournament model to have a BracketingMethod field.
     - Added a BracketingMethod model to indicate what algorithm should be used for bracketing.
    
    settings.py:
     - Added a line in settings to scan the templates folder for custom templates to use.
    
    hostSettings.py (+):
     - Added file hostSettings.py, which contains system specific variables (like absolute paths to important directories).

--- src/wgftm/wgftm/settings.py
+++ src/wgftm/wgftm/settings.py
@@ -1,10 +1,11 @@
 # Django settings for wgftm project.
+from hostSettings import CUSTOM_TEMPLATES_DIR
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Ryan George', 'ragmanme@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    CUSTOM_TEMPLATES_DIR
 )
 
 INSTALLED_APPS = (
@@ -116,9 +118,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
