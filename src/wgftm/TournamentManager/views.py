fc9343df8498e95ec5146ef0e09da71ef48afbca
rhyok <ragmanme@gmail.com>
2012-12-30 16:21:28 -0800

    + forms.py =====  - Added new file for keeping track of forms in the application. Currently contains a form for registration.
    views.py
    =====
     - Added the correct code for displaying the registration form. Registration form works, but there is no confirmation page. Need to add that. But it adds things to the database!
    
    urls.py
    =====
     - Updated urls.py with correct patterns and pages.
    
    models.py
    =====
     - Finalized models. Everything inside should work.
    
    database.db
    =====
     - Database synced to latest models.

--- src/wgftm/TournamentManager/views.py
+++ src/wgftm/TournamentManager/views.py
@@ -1,1 +1,46 @@
+from django.template import Context, loader
+from django.shortcuts import render
+from django.http import HttpResponse
+from TournamentManager.models import Player
+from TournamentManager.models import Guest
+from TournamentManager.forms import RegistrationForm
+from django.contrib.auth.models import User
+
 # Create your views here.
+#def login(request):
+   
+
+#def logout(request):
+   
+
+#def view_teams(request):
+   
+
+#def add_team(request):
+   
+
+#def tournament_detail(request):
+   
+
+#def view_tournaments(request):
+   
+
+def register(request):
+   if request.method == 'POST':
+      form = RegistrationForm(request.POST)
+      if form.is_valid():
+         formUser = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
+         formUser.first_name = form.cleaned_data['first_name']
+         formUser.last_name = form.cleaned_data['last_name']
+         if(form.cleaned_data['is_player']):
+            player = Player(user = formUser, isUcsd = form.cleaned_data['is_ucsd'], age=20, isBusy = False, ucsdCollege='ItsASecret', phoneNumber = '1234567890')
+            player.save()
+         else:
+            guest = Guest(user = formUser, isUcsd = form.cleaned_data['is_ucsd'])
+            guest.save()
+         formUser.save()
+         
+   else:
+      form = RegistrationForm()
+      
+   return render(request, 'tm_register.html', { 'form': form })
--- src/wgftm/wgftm/urls.py
+++ src/wgftm/wgftm/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.contrib import admin
+admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,10 +11,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wgftm.views.home', name='home'),
     # url(r'^wgftm/', include('wgftm.foo.urls')),
+    
+    # Tournament Manager URLs
+    url(r'^register/$', 'TournamentManager.views.register'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
