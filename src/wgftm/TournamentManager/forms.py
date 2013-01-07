69e017de30bf395d4cb37ddd1a3920f38527bdfd
rhyok <ragmanme@gmail.com>
2013-01-06 20:45:32 -0800

    TWO WEEKS REMAIN
    forms.py
    ======
    + added login form
    
    views.py
    ======
    + added methods for most of the major pages to the skeleton template pages
    
    tm_login.html
    ======
    + custom login template so we can skin it later on
    
    settings.py
    ======
    - changed login redirect to our redirect url
    
    urls.py
    ======
    + added url definitions for all app pages
    
    +tm_postlogout.html and tm_postregister.html
    ======
    + added these pages: landing pages after logout and register take place (basically, "Yes, you did a thing!" pages)
    
    +tm_viewuser.html
    ======
    + added this page for confirming that data was stored in the database. A basic profile page we can build off of.

--- src/wgftm/TournamentManager/forms.py
+++ src/wgftm/TournamentManager/forms.py
@@ -6,6 +6,10 @@ class RegistrationForm(forms.Form):
    last_name = forms.CharField(max_length=30)
    email = forms.EmailField()
    password = forms.CharField(widget=forms.PasswordInput(render_value=False), max_length=100)
-   is_player = forms.BooleanField(label="Check this box if you will be participating in a tournament")
+   is_player = forms.BooleanField(label="Check this box if you will be participating in a tournament", required=False)
    age = forms.IntegerField(min_value=1, max_value=150)
-   is_ucsd = forms.BooleanField(label="Check this box if you are a UCSD student")
+   is_ucsd = forms.BooleanField(label="Check this box if you are a UCSD student", required=False)
+   
+class LoginForm(forms.Form):
+   username = forms.CharField(max_length=30)
+   password = forms.CharField(widget=forms.PasswordInput(render_value=False), max_length=100)
--- src/wgftm/TournamentManager/views.py
+++ src/wgftm/TournamentManager/views.py
@@ -1,17 +1,59 @@
 from django.template import Context, loader
+
 from django.shortcuts import render
-from django.http import HttpResponse
-from TournamentManager.models import Player
-from TournamentManager.models import Guest
-from TournamentManager.forms import RegistrationForm
+
+from django.http import HttpResponse, HttpResponseRedirect
+
+from TournamentManager.forms import RegistrationForm, LoginForm
+from TournamentManager.models import Player, Guest
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-# Create your views here.
-#def login(request):
-   
 
-#def logout(request):
-   
+
+# Create your views here.
+def tm_login(request):
+   if request.method == 'POST':
+      form = LoginForm(request.POST)
+      username = request.POST['username']
+      password = request.POST['password']
+      user = authenticate(username=username, password=password)
+      if user is not None:
+         login(request, user)
+         return HttpResponseRedirect('/viewuser/')
+      else:
+         #Bad login
+         logout(request)
+         form = LoginForm()
+         return render(request, 'tm_login.html', { 'form' : form, 'status' : 'invalid' })  
+   else:
+      logout(request)
+      form = LoginForm()
+      return render(request, 'tm_login.html', { 'form' : form, 'next' : '/viewuser/' })
+
+def tm_logout(request):
+   logout(request)
+   return HttpResponseRedirect('/postlogout/')
+
+@login_required   
+def viewuser(request):
+   curUser = request.user
+   try:
+      player = Player.objects.get(user=curUser) 
+      guest = Guest.objects.get(user=curUser)
+   except Player.DoesNotExist:
+      player = None
+   except Guest.DoesNotExist:
+      guest = None
+   if player is not None:
+      usertype = 'player'
+      data = player
+   elif guest is not None:
+      usertype = 'guest'
+      data = guest
+   return render(request, 'tm_viewuser.html', { 'data' : data , 'user' : curUser, 'usertype' : usertype})
 
 #def view_teams(request):
    
@@ -23,8 +65,13 @@ from django.contrib.auth.models import User
    
 
 #def view_tournaments(request):
-   
+  
+def postregister(request):
+   return render(request, 'tm_postregister.html')
 
+def postlogout(request):
+   return render(request, 'tm_postlogout.html')
+  
 def register(request):
    if request.method == 'POST':
       form = RegistrationForm(request.POST)
@@ -33,13 +80,13 @@ def register(request):
          formUser.first_name = form.cleaned_data['first_name']
          formUser.last_name = form.cleaned_data['last_name']
          if(form.cleaned_data['is_player']):
-            player = Player(user = formUser, isUcsd = form.cleaned_data['is_ucsd'], age=20, isBusy = False, ucsdCollege='ItsASecret', phoneNumber = '1234567890')
+            player = Player(user = formUser, isUcsd = form.cleaned_data['is_ucsd'], age = form.cleaned_data['age'], isBusy = form.cleaned_data['age'], ucsdCollege='ItsASecret', phoneNumber = '1234567890')
             player.save()
          else:
             guest = Guest(user = formUser, isUcsd = form.cleaned_data['is_ucsd'])
             guest.save()
          formUser.save()
-         
+         return HttpResponseRedirect('/postregister/') # Redirect to thank you page
    else:
       form = RegistrationForm()
       
